fafe78d7eaee3e5123e447b227013e40fb5f487d
Bennyyoung <benjamineffiong124@yahoo.com>
2021-05-03 14:59:22 +0100

    Removed the exact link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,20 +13,20 @@ export default function Navbar() {
 
       <ul className="right hide-on-med-and-down">
        <li><NavLink exact to="/" activeStyle={{ color: 'green' }}>HOME</NavLink></li>
-       <li><NavLink exact to="/about-us" activeStyle={{ color: 'green' }}>ABOUT US</NavLink></li>
-       <li><NavLink exact to="/event" activeStyle={{ color: 'green' }}>OUR EVENT</NavLink></li>
-       <li><NavLink exact to="/team" activeStyle={{ color: 'green' }}>MEET THE TEAM</NavLink></li>
+       <li><NavLink to="/about-us" activeStyle={{ color: 'green' }}>ABOUT US</NavLink></li>
+       <li><NavLink to="/event" activeStyle={{ color: 'green' }}>OUR EVENT</NavLink></li>
+       <li><NavLink to="/team" activeStyle={{ color: 'green' }}>MEET THE TEAM</NavLink></li>
 
-       <li><NavLink exact to="/contact" activeStyle={{ color: 'green' }}>APPLY</NavLink></li>
+       <li><NavLink to="/contact" activeStyle={{ color: 'green' }}>APPLY</NavLink></li>
       </ul>
 
       <ul id="nav-mobile" className="sidenav">
        <li><NavLink exact to="/" activeStyle={{ color: 'green' }}>HOME</NavLink></li>
-       <li><NavLink exact to="/about-us" activeStyle={{ color: 'green' }}>ABOUT</NavLink></li>
-       <li><NavLink exact to="/event" activeStyle={{ color: 'green' }}>OUR EVENT</NavLink></li>
-       <li><NavLink exact to="/team" activeStyle={{ color: 'green' }}>MEET THE TEAM</NavLink></li>
+       <li><NavLink to="/about-us" activeStyle={{ color: 'green' }}>ABOUT</NavLink></li>
+       <li><NavLink to="/event" activeStyle={{ color: 'green' }}>OUR EVENT</NavLink></li>
+       <li><NavLink to="/team" activeStyle={{ color: 'green' }}>MEET THE TEAM</NavLink></li>
 
-       <li><NavLink exact to="/contact" activeStyle={{ color: 'green' }}>APPLY</NavLink></li>
+       <li><NavLink to="/contact" activeStyle={{ color: 'green' }}>APPLY</NavLink></li>
       </ul>
 
       <Link to="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
